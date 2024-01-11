5d8b082c452c3e82ae537c7e03870ba0121d7a75
diurno <diurno@B21>
2024-01-11 13:35:46 +0100

    Versión2

--- Proyecto1/src/Paquete1/Principal.java
+++ Proyecto1/src/Paquete1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generate method stub
 		System.out.println("Ejemplo de contro de versiones con Git");
-		
+		System.out.println("Segundo ejemplo del control de versiones");
 	}
 
 }
